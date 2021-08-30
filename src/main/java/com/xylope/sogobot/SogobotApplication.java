7c4f1bbc7c849445c4538094aea1542d810d484a
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-08-31 08:18:35 +0900

    Initial commit

---
+++ src/main/java/com/xylope/sogobot/SogobotApplication.java
@@ -0,0 +1,13 @@
+package com.xylope.sogobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SogobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SogobotApplication.class, args);
+    }
+
+}
